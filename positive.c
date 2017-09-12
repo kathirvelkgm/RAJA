f1896e8d69629017aa99bce40498200ae761db06
kathirvelkgm <31503336+kathirvelkgm@users.noreply.github.com>
2017-09-12 16:05:53 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+int a;
+if(a>=0)
+{
+printf("n");
+}
+if else(n<=0)
+{
+printf("n");
+}
+else
+{
+printf("nothing is given as integer");
+}
+}
+© 2017 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+API
+Training
+Shop
+Blog
